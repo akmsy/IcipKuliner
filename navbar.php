7cd65a4b0057481f585ce6619eaccdb52a15dc14
akmsy <akmsyyy1@gmail.com>
2026-04-25 09:47:04 +0700

    perbaiki kondisi login di navbar yang lebih simple, memperbaiki method form search, tambah name buat input search

--- navbar.php
+++ navbar.php
@@ -1,50 +1,37 @@
 <?php 
-    if (!$login) {
-        ?>
-        <nav class="navbar bg-body-light shadow-sm">
-            <div class="container-fluid">
-                <div class="d-flex align-items-center">
-                    <img src="img/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top me-2">
-                    <a class="navbar-brand fw-bold" href="index.php">IcipKuliner</a>
-                </div>
-
-                <div class="d-flex">
-                    <a href="index.php" class="nav-link me-3">Home</a>
-                    <a href="explore.php" class="nav-link me-3">Explore</a>
-                    <a href="contribute.php" class="nav-link">Contribute</a>
-                </div>
-
-                <div class="d-flex">
-                    <a href="login.php" class="btn btn-outline-orange rounded rounded-pill px-4 me-3">Login</a>
-                    <a href="register.php" class="btn btn-orange rounded-pill px-4">Register</a>
-                </div>
-            </div>
-        </nav>
-    <?php 
-    } 
-    else {
-        ?>
-        <nav class="navbar bg-body-light">
-            <div class="container-fluid">
-                <div class="d-flex align-items-center">
-                    <img src="img/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top me-2">
-                    <a class="navbar-brand fw-bold" href="index.php">IcipKuliner</a>
-                </div>
-
-                <div class="d-flex">
-                    <a href="index.php" class="nav-link me-3">Home</a>
-                    <a href="explore.php" class="nav-link me-3">Explore</a>
-                    <a href="contribute.php" class="nav-link">Contribute</a>
-                </div>
-
-                <div class="d-flex align-items-center">
-                    <form class="form-inline my-2 my-lg-0" action="search.php" method="$_GET">
-                        <input class="form-control mr-sm-2 rounded-pill" type="search" placeholder="Search" aria-label="Search">
-                    </form>
-                    <img src="img/profil.png" alt="Profile" width="40" height="40" class="rounded-circle ms-2">
-                </div>
-            </div>
-        </nav>
-    <?php
-    }
+session_start();
+include 'koneksi.php';
 ?>
+
+<nav class="navbar bg-body-light shadow-sm">
+    <div class="container-fluid">
+        
+        <div class="d-flex align-items-center">
+            <img src="img/logo.png" width="50" height="50" class="me-2">
+            <a class="navbar-brand fw-bold" href="index.php">IcipKuliner</a>
+        </div>
+
+        <div class="d-flex">
+            <a href="index.php" class="nav-link me-3">Home</a>
+            <a href="explore.php" class="nav-link me-3">Explore</a>
+            <a href="contribute.php" class="nav-link">Contribute</a>
+        </div>
+
+        <div class="d-flex align-items-center">
+        <?php if (!isset($_SESSION['logged_in'])): ?>
+
+            <a href="login.php" class="btn btn-outline-orange rounded-pill px-4 me-3">Login</a>
+            <a href="register.php" class="btn btn-orange rounded-pill px-4">Register</a>
+
+        <?php else: ?>
+
+            <form action="search.php" method="GET">
+                <input class="form-control rounded-pill" type="search" name="keyword" placeholder="Search">
+            </form>
+            <img src="img/profil.png" width="40" height="40" class="rounded-circle ms-2">
+
+        <?php endif; ?>
+        </div>
+
+    </div>
+</nav>
