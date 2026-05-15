6aec95bcb1356902c03364ad4c7a4cbf0a8f006b
akmsy <akmsyyy1@gmail.com>
2026-05-15 10:48:28 +0700

    bikin verifikasi buat login

--- proses_login.php
+++ proses_login.php
@@ -4,19 +4,34 @@
 
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+
+    if (empty($email) || empty($password)) {
+        $_SESSION['login_error'] = "Email dan password wajib diisi.";
+        header("location: login.php");
+        exit();
+    }
+
+    $query = "SELECT * FROM users WHERE email='$email'";
     
     $result = mysqli_query($koneksi,$query);
 
     if (mysqli_num_rows($result) > 0) {
         $user = mysqli_fetch_assoc($result);
-        $_SESSION['id'] = $user['id'];
-        $_SESSION['email'] = $user['email'];
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['logged_in'] = true;
-        header("location: index.php");
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['id']        = $user['id'];
+            $_SESSION['email']     = $user['email'];
+            $_SESSION['username']  = $user['username'];
+            $_SESSION['logged_in'] = true;
+            header("location: index.php");
+            exit();
+        } else {
+            $_SESSION['login_error'] = "Email atau password salah";
+            header("location: login.php");
+            exit();
+        }
     } else {
         $_SESSION['login_error'] = "Email atau password salah";
         header("location: login.php");
+        exit();
     }
 ?>
