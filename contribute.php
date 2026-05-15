7a35a186dfb29d73ad2df797bab6387c3c1265e5
akmsy <akmsyyy1@gmail.com>
2026-05-16 00:50:26 +0700

    optimize tabel kontribusi

--- contribute.php
+++ contribute.php
@@ -76,20 +76,18 @@
         $total_page = ceil($total_data / $limit);
     ?>
  
-    <section class="table-section">
-
+    <section class="table-section mt-5">
         <div class="table-header">
-            <p>NAMA TEMPAT</p>
-            <p>LOKASI</p>
-            <p>KATEGORI</p>
-            <p>ULASAN</p>
-            <p>AKSI</p>
+            <span>NAMA TEMPAT</span>
+            <span>LOKASI</span>
+            <span>KATEGORI</span>
+            <span>ULASAN</span>
+            <span>AKSI</span>
         </div>
 
         <?php while($data = mysqli_fetch_assoc($query)){?>
 
         <div class="table-row">
-
             <div class="spot-info">
                 <img src="upload/<?= $data['gambar']; ?>" class="spot-img">
                 <div>
@@ -111,7 +109,7 @@
                 (<?= $data['total_ulasan']; ?> ulasan)
             </div>
 
-            <div>
+            <div class="action-cell">
                 <a href="edit.php?id=<?= $data['id_kuliner'];?>" class="btn btn-success">Edit</a>
                 <a href="hapus.php?id=<?= $data['id_kuliner']; ?>" class="btn btn-warning" onclick="return confirm('Yakin ingin menghapus data ini?')">Hapus</a>
             </div>
@@ -139,183 +137,183 @@
     </section>
 
     <!-- Form -->
-<section class="main-container">
-    <section class="title-section text-center">
-        <h1>Formulir Rekomendasi</h1>
-        <p>Daftarkan pusaka kuliner lokal ke dalam editorial kami.</p>
-    </section>
+    <section class="main-container">
+        <section class="title-section text-center">
+            <h1>Formulir Rekomendasi</h1>
+            <p>Daftarkan pusaka kuliner lokal ke dalam editorial kami.</p>
+        </section>
 
-    <section class="form-card" id="form-contribute">
-        <form action="proses_contribute.php" method="POST" enctype="multipart/form-data">
-            <div class="row">
-                <div class="col-md-6 mb-4">
-                    <label class="form-label custom-label">NAMA TEMPAT</label>
-                    <input
-                    type="text"
-                    name="place"
-                    class="form-control custom-input"
-                    placeholder="e.g. Warung Nasi Campur Bu Made"
-                    required>
-                </div>
-            
-                <div class="col-md-6 mb-4">
-                    <label class="form-label custom-label">KATEGORI</label>
-                    <select name="kategori" class="form-select custom-input" required>
-                        <option value="" selected disabled>Pilih Kategori</option>
-                        <?php
-                            $kategoriQuery = mysqli_query($koneksi,"
-                                SELECT *
-                                FROM kategori
-                            ");
-
-                            while($kat = mysqli_fetch_assoc($kategoriQuery)){
-                        ?>
-                        <option value="<?= $kat['id_kategori']; ?>">
-                            <?= $kat['nama_kategori']; ?>
-                        </option>
-                        <?php } ?>
-                    </select>
-                </div>
-                
-                <div class="col-md-6 mb-4">
-                    <label class="form-label custom-label">DETAIL ALAMAT</label>
-                    <input
-                    type="text"
-                    name="detail_alamat"
-                    class="form-control custom-input"
-                    placeholder="Contoh: Jl. Malioboro No 10"
-                    required>
-                </div>
-
-                <div class="col-md-6 mb-4">
-                    <label class="form-label custom-label">KABUPATEN</label>
-                    <select id="kabupaten" name="kabupaten" class="form-select custom-input" required>
-                        <option value="" selected disabled>Pilih Kabupaten</option>
-                        <?php 
-                            $query = mysqli_query($koneksi, "SELECT id_kabupaten, nama_kabupaten FROM kabupaten");
-                            while ($data = mysqli_fetch_array($query)){
-                        ?>
-                        <option value="<?= $data['id_kabupaten'] ?>"><?= $data['nama_kabupaten'] ?></option>
-                        <?php 
-                            }
-                        ?>
-                    </select>
-                </div>
-            </div>
-    
-            <div class="row">
-                <div class="col-md-6 mb-4">
-                    <label class="form-label custom-label">KECAMATAN</label>
-                    <select id="kecamatan" name="kecamatan" class="form-select custom-input" required>
-                        <option value="" selected disabled>Pilih Kecamatan</option>
-                    </select>
-                    
-                </div>
-                
-                <div class="col-md-6 mb-4">
-                    <label class="form-label custom-label">DESA</label>
-                    <select id="desa" name="desa" class="form-select custom-input" required>
-                        <option value="" selected disabled>Pilih Desa</option>
-                    </select>
-                    
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6 mb-4">
-                    <label class="form-label custom-label">
-                        JAM OPERASIONAL
-                    </label>
-                    <input
-                    type="text"
-                    name="jam_operasional"
-                    class="form-control custom-input"
-                    placeholder="Contoh: 08.00 - 22.00"
-                    required>
-                </div>
-                <div class="col-md-6 mb-4">
-                    <label class="form-label custom-label">
-                        HARGA
-                    </label>
-                    <input
-                    type="text"
-                    name="harga"
-                    class="form-control custom-input"
-                    placeholder="Contoh: Rp15.000 - Rp50.000"
-                    required>
-                </div>
-            </div>
-
-            <div class="mb-4">
-                <label class="form-label custom-label">
-                    LINK GOOGLE MAPS
-                </label>
-                <input
-                type="url"
-                name="maps"
-                class="form-control custom-input"
-                placeholder="https://maps.google.com/..."
-                required>
-            </div>
-
-            <div class="mb-4">
-                <label class="form-label custom-label">
-                    RATING
-                </label>
-                <select
-                name="rating"
-                class="form-select custom-input"
-                required>
-                    <option value="" selected disabled>
-                        Pilih Rating
-                    </option>
-                    <option value="1">1 ⭐</option>
-                    <option value="2">2 ⭐⭐</option>
-                    <option value="3">3 ⭐⭐⭐</option>
-                    <option value="4">4 ⭐⭐⭐⭐</option>
-                    <option value="5">5 ⭐⭐⭐⭐⭐</option>
-                </select>
-            </div>
-
-            <div class="mb-4">
-                <label class="form-label custom-label">
-                    ULASAN
-                </label>
-                <textarea
-                name="review"
-                class="form-control custom-textarea"
-                placeholder="Bagikan pengalamanmu..."
-                required></textarea>
-            </div>
-
-            <div class="mb-4">
-                <label class="form-label custom-label" for="image-upload">FOTO TEMPAT</label>
-                <div class="upload-box">
-                    <label for="image-upload" class="upload-content">
-                        <h5>Klik untuk mengunggah atau seret foto</h5>
-                        <p>Format JPG, PNG Max. 5MB. Rekomendasi aspek rasio 4:3</p>
-
+        <section class="form-card" id="form-contribute">
+            <form action="proses_contribute.php" method="POST" enctype="multipart/form-data" class="align-center">
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <label class="form-label custom-label">NAMA TEMPAT</label>
                         <input
-                        type="file"
-                        id="image-upload"
-                        name="image"
-                        accept=".jpg,.jpeg,.png,.webp"
-                        hidden>
-                    </label>
+                        type="text"
+                        name="place"
+                        class="form-control custom-input"
+                        placeholder="e.g. Warung Nasi Campur Bu Made"
+                        required>
+                    </div>
+                
+                    <div class="col-md-6 mb-4">
+                        <label class="form-label custom-label">KATEGORI</label>
+                        <select name="kategori" class="form-select custom-input" required>
+                            <option value="" selected disabled>Pilih Kategori</option>
+                            <?php
+                                $kategoriQuery = mysqli_query($koneksi,"
+                                    SELECT *
+                                    FROM kategori
+                                ");
+
+                                while($kat = mysqli_fetch_assoc($kategoriQuery)){
+                            ?>
+                            <option value="<?= $kat['id_kategori']; ?>">
+                                <?= $kat['nama_kategori']; ?>
+                            </option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    
+                    <div class="col-md-6 mb-4">
+                        <label class="form-label custom-label">DETAIL ALAMAT</label>
+                        <input
+                        type="text"
+                        name="detail_alamat"
+                        class="form-control custom-input"
+                        placeholder="Contoh: Jl. Malioboro No 10"
+                        required>
+                    </div>
+
+                    <div class="col-md-6 mb-4">
+                        <label class="form-label custom-label">KABUPATEN</label>
+                        <select id="kabupaten" name="kabupaten" class="form-select custom-input" required>
+                            <option value="" selected disabled>Pilih Kabupaten</option>
+                            <?php 
+                                $query = mysqli_query($koneksi, "SELECT id_kabupaten, nama_kabupaten FROM kabupaten");
+                                while ($data = mysqli_fetch_array($query)){
+                            ?>
+                            <option value="<?= $data['id_kabupaten'] ?>"><?= $data['nama_kabupaten'] ?></option>
+                            <?php 
+                                }
+                            ?>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            
-            <div class="button-section">
-                <button type="button" class="cancel-btn">
-                    Batalkan
-                </button>
+        
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <label class="form-label custom-label">KECAMATAN</label>
+                        <select id="kecamatan" name="kecamatan" class="form-select custom-input" required>
+                            <option value="" selected disabled>Pilih Kecamatan</option>
+                        </select>
+                        
+                    </div>
+                    
+                    <div class="col-md-6 mb-4">
+                        <label class="form-label custom-label">DESA</label>
+                        <select id="desa" name="desa" class="form-select custom-input" required>
+                            <option value="" selected disabled>Pilih Desa</option>
+                        </select>
+                        
+                    </div>
+                </div>
 
-                <button type="submit" class="submit-btn">
-                    Simpan Kontribusi
-                </button>
-            </div>
-        </form>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <label class="form-label custom-label">
+                            JAM OPERASIONAL
+                        </label>
+                        <input
+                        type="text"
+                        name="jam_operasional"
+                        class="form-control custom-input"
+                        placeholder="Contoh: 08.00 - 22.00"
+                        required>
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <label class="form-label custom-label">
+                            HARGA
+                        </label>
+                        <input
+                        type="text"
+                        name="harga"
+                        class="form-control custom-input"
+                        placeholder="Contoh: Rp15.000 - Rp50.000"
+                        required>
+                    </div>
+                </div>
 
+                <div class="mb-4">
+                    <label class="form-label custom-label">
+                        LINK GOOGLE MAPS
+                    </label>
+                    <input
+                    type="url"
+                    name="maps"
+                    class="form-control custom-input"
+                    placeholder="https://maps.google.com/..."
+                    required>
+                </div>
+
+                <div class="mb-4">
+                    <label class="form-label custom-label">
+                        RATING
+                    </label>
+                    <select
+                    name="rating"
+                    class="form-select custom-input"
+                    required>
+                        <option value="" selected disabled>
+                            Pilih Rating
+                        </option>
+                        <option value="1">1 ⭐</option>
+                        <option value="2">2 ⭐⭐</option>
+                        <option value="3">3 ⭐⭐⭐</option>
+                        <option value="4">4 ⭐⭐⭐⭐</option>
+                        <option value="5">5 ⭐⭐⭐⭐⭐</option>
+                    </select>
+                </div>
+
+                <div class="mb-4">
+                    <label class="form-label custom-label">
+                        ULASAN
+                    </label>
+                    <textarea
+                    name="review"
+                    class="form-control custom-textarea"
+                    placeholder="Bagikan pengalamanmu..."
+                    required></textarea>
+                </div>
+
+                <div class="mb-4">
+                    <label class="form-label custom-label" for="image-upload">FOTO TEMPAT</label>
+                    <div class="upload-box">
+                        <label for="image-upload" class="upload-content">
+                            <h5>Klik untuk mengunggah atau seret foto</h5>
+                            <p>Format JPG, PNG Max. 5MB. Rekomendasi aspek rasio 4:3</p>
+
+                            <input
+                            type="file"
+                            id="image-upload"
+                            name="image"
+                            accept=".jpg,.jpeg,.png,.webp"
+                            hidden>
+                        </label>
+                    </div>
+                </div>
+                
+                <div class="button-section">
+                    <button type="button" class="cancel-btn">
+                        Batalkan
+                    </button>
+
+                    <button type="submit" class="submit-btn">
+                        Simpan Kontribusi
+                    </button>
+                </div>
+            </form>
+        </section>
     </section>
 
 </main>
